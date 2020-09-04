a98508da9761e7f885603dd0e19d9830e0235688
armchair-traveller <sentient.sober.armchair@gmail.com>
2020-09-04 12:15:02 -0700

    style

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,27 @@
 <script>
   import heroes from "../utils/heroes";
   import getChains from "../utils/get-chains";
+  import { fade } from "svelte/transition";
 
   let value = "";
+
+  let isVisible = true;
 
   var heroNames = Object.keys(heroes);
   var allChains = getChains(heroes);
   $: chains = allChains.filter((chain) => {
-    return chain.find((el) => el == "hekate");
+    return chain.find((el) => el);
   });
 </script>
 
 <style>
   h1,
-  p {
+  p,
+  h3 {
     text-align: center;
     margin: 0 auto;
+    color: var(--primary-color);
   }
-
   h1 {
     font-size: 2.8em;
     text-transform: uppercase;
@@ -26,6 +30,8 @@
   }
 
   p {
+    /* override cascade */
+    color: var(--secondary-color);
     margin: 1em auto;
   }
 
@@ -37,21 +43,24 @@
 
   section {
     display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    flex-flow: row wrap;
   }
 
   .container {
-    background-color: #dce3e9;
     width: 100%;
+    height: 100%;
     display: flex;
     flex-flow: column;
     align-items: center;
   }
 
   input {
-    margin-top: 2.5em;
     border: none;
     padding: 0.5em 1em;
     border-radius: 1em;
+    margin: 1.5em auto;
   }
   .outline {
     outline: none;
@@ -59,6 +68,32 @@
   }
   .outline:focus {
     box-shadow: 0 0 0 3px rgba(66, 153, 225, 0.6);
+  }
+
+  .stack > * + * {
+    margin-top: 1.5rem;
+  }
+
+  h3 {
+    font-weight: 500;
+  }
+
+  br {
+    width: 7em;
+    height: 0.1em;
+    margin-bottom: 1em;
+    content: "";
+    border-top: 1px solid var(--primary-color);
+  }
+
+  .arrow {
+    color: #fff;
+    margin-left: 1em;
+    margin-right: 1em;
+  }
+
+  .chain {
+    white-space: nowrap;
   }
 </style>
 
@@ -82,18 +117,43 @@
         }
       }} />
   </div>
-  {#each heroNames.filter((v) => v.includes(value)) as hero}
-    <p>{hero[0].toUpperCase() + hero.slice(1)}</p>
-  {/each}
+  <!-- TODO: Make this into a search dropdown -->
+  {#if false}
+    {#each heroNames.filter((v) => v.includes(value)) as hero}
+      <p>{hero[0].toUpperCase() + hero.slice(1)}</p>
+    {/each}
+  {/if}
 
-  {#each chains as chain}
-    <section>
-      {#each chain as heroName}
-        <div>
-          <p>{heroName}</p>
-          <p>{heroes[heroName].chain[0]} -> {heroes[heroName].chain[1]}</p>
-        </div>
-      {/each}
-    </section>
-  {/each}
+  <button on:click={() => (isVisible = !isVisible)}>Click me</button>
+
+  {#if isVisible}
+    {#each chains as chain, i}
+      <section transition:fade>
+        {#each chain as heroName, i}
+          <!-- Arrow logic -->
+          {#if i != 0}
+            <p class="arrow">
+              {#if heroes[chain[i - 1]].chain[1] == heroes[heroName].chain[0]}
+                &Rarr;
+              {:else}&rarr;{/if}
+            </p>
+          {/if}
+          <!-- Hero & chain -->
+          <div>
+            <h3>{heroName}</h3>
+            <p class="chain">
+              {#if i == 0}
+                {heroes[heroName].chain[0]} / {heroes[heroName].chain[1]}
+              {:else if heroes[chain[i - 1]].chain[1] == heroes[heroName].chain[0]}
+                {heroes[heroName].chain[1]}
+              {:else}
+                {heroes[heroName].chain[0]} / {heroes[heroName].chain[1]}
+              {/if}
+            </p>
+          </div>
+        {/each}
+      </section>
+      {#if i != chains.length - 1}<br />{/if}
+    {/each}
+  {/if}
 </div>
